e8bfa8dc42d87900586b2b469ca4a13ec841730b
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2023-08-10 13:55:08 -0500

    update app index

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-
-;
-
-// function getpokeinfo(pokename){
-//     axios.get(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`)
-//     .then(response => {
-//         pokeList = response.data.results;
-//         // console.log(pokeList);
-
-//         pokeList.forEach(x => {
-//             pokeIndex[x.name] = x;
-//         }); //Index the array with the name 
-
-//         // console.log(pokeIndex);
-
-//         let pokemon = pokeIndex[`${pokename}`];
-//         console.log(pokemon.url)
-//     });
 const pokeIndex = []
-const dataList = document.getElementById('pokemon-list');
+const datalist = document.getElementById('pokemon-list');
 
-async function getpokeinfo(pokename){
+async function getpokeinfo(){
     try {
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`);
+
+        // console.log("data",response.data)
 
         pokeList = response.data.results;
         // console.log("pokeList",pokeList);
@@ -30,25 +14,19 @@ async function getpokeinfo(pokename){
         pokeIndex[x.name] = x;
         }); //Index the array with the name 
 
-        console.log("pokeIndex",pokeIndex);
-
-        let pokemon = pokeIndex[`${pokename}`];
-        console.log("pokemon",pokemon.url)
-
+        // console.log("pokeIndex",pokeIndex);
         return pokeIndex;
 
     } catch (error) {
         console.error(error);
-        alert("Pokemon is not in Data-List")
+        alert("It was not possible to access data-base")
     }
     
-    }
+    };
 
 
-async function Send(){
-    const EnterPoke = document.getElementById("name").value.toLowerCase();
-    console.log(EnterPoke);
-    await getpokeinfo(EnterPoke);
+async function List(){
+    await getpokeinfo();
     // console.log("pokeIndex",pokeIndex);
     const pokemonNames = Object.keys(pokeIndex);
     // console.log("pokemonNames",pokemonNames);
@@ -56,64 +34,124 @@ async function Send(){
     pokemonNames.forEach(x => {
         const option = document.createElement("option");
         option.value = x;
-        dataList.appendChild(option);
+        datalist.appendChild(option);
     })
 }
 
+function pokeId(){
+    const EnterPoke = document.getElementById("name").value.toLowerCase();
 
-async function getData(pokename){
+    PokeUrl = pokeIndex[EnterPoke].url;
+    console.log("PokeUrl", EnterPoke, PokeUrl);
+    
+}
+
+async function stats(pokeid){
     try {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokename}/`);
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeid}/`);
         
-        let myArray = [];
+
+        let pokeStats = [];
         
         for(let i = 0; i< response.data.stats.length; i++){
             let myObject = {[response.data.stats[i].stat.name] : response.data.stats[i].base_stat}
-            myArray.push(myObject); 
+            pokeStats.push(myObject); 
         }
-        console.log(myArray)
+        // console.log("pokeStats",pokeStats)
+        // console.log(response.data.forms[0].url)
+        // console.log(response.data.name)
 
-        console.log(response.data.forms[0].url)
-
+        return pokeStats;
     } catch (error) {
         console.error(error);
         alert("Pokemon is not in Data-List")
     }
 }
 
-pokename = 25
-getData(pokename)
+// pokeid = 25
+// stats(pokeid)
 
 
-async function getImg(pokename){
+
+async function gett(){
+    let pokeMasterList = [];
+    let pokeMList = [];
     try {
-        const response = await axios.get(`https://pokeapi.co/api/v2/version-group/${pokename}/`);
-        
-        console.log("IMG: ",response.data)
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`);
+
+        let numberOfPoke = response.data.count;
+        // console.log("Number of Poke:", numberOfPoke);
+        const results = response.data.results;
+        // console.log("Results:", results);
+
+        results.map(poke => {
+            // console.log(poke)
+            let urlParts = poke.url.split('/');
+            // Get the penultimate element
+            let idStr = urlParts[urlParts.length - 2];
+            // Convert the string to an integer
+            let id = parseInt(idStr);
+            // console.log("ID:", id);
+
+            poke.id = id;
+            poke.img1 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+            poke.img2 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+            // console.log(poke)
+            pokeMList.push(poke)
+        })
+
+        pokeMList.map(poke => {
+            pokeMasterList[poke.name] = poke
+        })
+
+        // console.log("Pokemon Master List:", pokeMList)
+        // console.log("viewccc", pokeMasterList)
+        console.log("viewccc", pokeMasterList)
+
+        return pokeMasterList;
 
     } catch (error) {
         console.error(error);
-        alert("Pokemon is not in Data-List")
+        alert("don´t get")
     }
 }
 
-getImg(pokename);
+let pokeMasterList = gett()
 
-// abomasnow
-    
+async function lastList(){
+
+    try {
+        let pokeMasterList = await gett();
+        console.log("Last List", pokeMasterList);
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/`);
+        console.log("Last List", response.data.results[2]);
+
+    } catch (error) {
+        console.error(error);
+        alert("Last List error")
+    }
+}
+
+lastList()
 
 
 
 
-// axios.get(`https://pokeapi.co/api/v2/`)
-// .then(response => {
-//     console.log(response)
-// })
+// let poke = { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/2333333/' };
 
-// const animales = ['Elephant', 'Lion', 'Giraffe', 'Tiger', 'Zebra', 'Monkey', 'Bear', 'Kangaroo', 'Penguin', 'Koala'];
-// const dataList = document.getElementById('animal-list');
+// // Divide the URL in parts using '/'
+// let urlParts = poke.url.split('/');
 
-// animales.forEach(animal => {
-//     const option = document.createElement('option');
-//     option.value = animal;
-//     dataList.appendChild(option);
+// // Get the penultimate element
+// let idStr = urlParts[urlParts.length - 2];
+
+// // Convert the string to an integer
+// let id = parseInt(idStr);
+
+// console.log("ID:", id);
+
+// poke.id = id;
+
+// // console.log("ID:", id);
+// // console.log("Modified Pokemon:", poke);
+
